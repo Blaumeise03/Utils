72d62a7f98b7cf995870374d0ac1290a136611b4
Blaumeise03 <bluegame61@gmail.com>
2019-09-29 12:06:38 +0200

    Updated HttpRequest
    Signed-off-by: Blaumeise03 <bluegame61@gmail.com>

--- src/main/java/de/blaumeise03/utils/http/HttpRequest.java
+++ src/main/java/de/blaumeise03/utils/http/HttpRequest.java
@@ -24,18 +24,33 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
+/**
+ * Class for easy-http-requests
+ */
 public class HttpRequest {
-    private int responseCode;
-    private String response;
-    private HttpURLConnection connection;
+    private int responseCode = -1;
+    private String response = null;
+    private HttpURLConnection connection = null;
     private String url;
     private Map<String,String> headerParameters;
+    private IOException exception = null;
 
+    /**
+     * Constructor for HttpRequest.
+     *
+     * @param url              the target URL
+     * @param headerParameters all parameters for the header (e.g. "User-Agent: Java-Client")
+     */
     public HttpRequest(String url, Map<String,String> headerParameters){
         this.url = url;
         this.headerParameters = headerParameters;
     }
 
+    /**
+     * Method for executing the GET request. If the request fails with an exception it will return false, you can access the exception with {@link HttpRequest#getException()}.
+     *
+     * @return true if the request is successfully complete.
+     */
     public boolean executeGet(){
         responseCode = -1;
         response = null;
@@ -65,10 +80,64 @@ public class HttpRequest {
             }
         } catch (IOException e) {
             //e.printStackTrace();
+            exception = e;
             return false;
         }
 
         return true;
     }
 
+    /**
+     * Getter for the response-code (if {@link HttpRequest#executeGet()} is not executed before, the response-code is -1)
+     *
+     * @return the response-code
+     */
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    /**
+     * Getter for the response (if {@link HttpRequest#executeGet()} is not executed before, the response is null)
+     *
+     * @return the response-message
+     */
+    public String getResponse() {
+        return response;
+    }
+
+    /**
+     * Getter for the {@link HttpURLConnection}.
+     *
+     * @return the {@link HttpURLConnection}
+     */
+    public HttpURLConnection getConnection() {
+        return connection;
+    }
+
+    /**
+     * Getter for the URL
+     *
+     * @return the URL of the request
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Getter for the header-parameters
+     *
+     * @return the header-parameters
+     */
+    public Map<String, String> getHeaderParameters() {
+        return headerParameters;
+    }
+
+    /**
+     * Getter for the exception (only if {@link HttpRequest#executeGet()} returns false)
+     *
+     * @return the {@link IOException, IOException} if {@link HttpRequest#executeGet()} returns false, else it will return null.
+     */
+    public IOException getException() {
+        return exception;
+    }
 }
